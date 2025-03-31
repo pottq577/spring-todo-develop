f276be4e025cca34034591de96f6265620fac058
hyun2y00 <pottq577@naver.com>
2025-03-31 14:44:46 +0900

    initial commit

---
+++ src/main/java/com/example/springtododevelop/SpringTodoDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtododevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTodoDevelopApplication.class, args);
+    }
+
+}
