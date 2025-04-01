bc62b11875de322144c1c9294d0c68f23fe23685
hyun2y00 <pottq577@naver.com>
2025-04-01 11:38:53 +0900

    Feat: [Lv2] 유저 서비스 설계

---
+++ src/main/java/com/example/springtododevelop/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.springtododevelop.service;
+
+import com.example.springtododevelop.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+}
