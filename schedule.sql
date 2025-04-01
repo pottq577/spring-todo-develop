a7c104fd79d93f9cea38bf9e555208af87ee1a7c
hyun2y00 <pottq577@naver.com>
2025-04-01 19:25:53 +0900

    Fix: 일정의 제목과 내용 컬럼을 가지도록 sql 수정

--- schedule.sql
+++ schedule.sql
@@ -11,11 +11,12 @@ CREATE TABLE users
 
 CREATE TABLE schedules
 (
-    schedule_id BIGINT    NOT NULL AUTO_INCREMENT COMMENT '일정 식별자 ID',
-    user_id     BIGINT    NOT NULL COMMENT '유저 식별자 ID',
-    todo        TEXT      NULL COMMENT '일정 내용',
-    createdAt   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '일정 등록 날짜',
-    updatedAt   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '일정 수정 날짜',
+    schedule_id BIGINT      NOT NULL AUTO_INCREMENT COMMENT '일정 식별자 ID',
+    user_id     BIGINT      NOT NULL COMMENT '유저 식별자 ID',
+    title       VARCHAR(50) NOT NULL COMMENT '일정 제목',
+    contents    TEXT        NOT NULL COMMENT '일정 내용',
+    createdAt   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '일정 등록 날짜',
+    updatedAt   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '일정 수정 날짜',
     PRIMARY KEY (schedule_id),
     FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
