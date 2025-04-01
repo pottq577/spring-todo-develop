ba4260766f0ad694c7e045fd7511cd43f0f90f7e
hyun2y00 <pottq577@naver.com>
2025-04-01 14:05:06 +0900

    Feat: [Lv2] 유저 전체 조회 컨트롤러 구현 및 API 설계 완료

--- src/main/java/com/example/springtododevelop/controller/UserController.java
+++ src/main/java/com/example/springtododevelop/controller/UserController.java
@@ -3,9 +3,11 @@ package com.example.springtododevelop.controller;
 import com.example.springtododevelop.dto.users.UserCreateRequestDto;
 import com.example.springtododevelop.dto.users.UserResponseDto;
 import com.example.springtododevelop.service.UserService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,15 @@ public class UserController {
             );
 
         return new ResponseEntity<>(userResponseDto, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponseDto>> findAllUsers() {
+
+        List<UserResponseDto> users = userService.findAllUsers();
+
+        return new ResponseEntity<>(users, HttpStatus.OK);
 
     }
 
