6df8485c16c2628c2efc2db39bc8dae443e3dcdb
hyun2y00 <pottq577@naver.com>
2025-04-01 13:57:29 +0900

    Feat: [Lv2] 유저 전체 조회 서비스 구현

--- src/main/java/com/example/springtododevelop/service/UserService.java
+++ src/main/java/com/example/springtododevelop/service/UserService.java
@@ -3,6 +3,8 @@ package com.example.springtododevelop.service;
 import com.example.springtododevelop.dto.users.UserResponseDto;
 import com.example.springtododevelop.entity.Users;
 import com.example.springtododevelop.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +19,23 @@ public class UserService {
         Users user = new Users(username, password, email);
         Users createdUser = userRepository.save(user);
 
-        return new UserResponseDto(
-            createdUser.getUserId(),
-            createdUser.getUsername(),
-            createdUser.getEmail(),
-            createdUser.getCreatedAt(),
-            createdUser.getUpdatedAt());
+        return new UserResponseDto(createdUser.getUserId(), createdUser.getUsername(),
+            createdUser.getEmail(), createdUser.getCreatedAt(), createdUser.getUpdatedAt());
+
+    }
+
+    public List<UserResponseDto> findAllUsers() {
+
+        List<Users> user = userRepository.findAll();
+        List<UserResponseDto> userResponseDtoList = new ArrayList<>();
+
+        for (Users users : user) {
+            userResponseDtoList.add(
+                new UserResponseDto(users.getUserId(), users.getUsername(), users.getPassword(),
+                    users.getCreatedAt(), users.getUpdatedAt()));
+        }
+
+        return userResponseDtoList;
 
     }
 
