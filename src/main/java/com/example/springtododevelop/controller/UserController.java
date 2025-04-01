db4f213d08d533ea8918064258aa6c69b1dd1131
hyun2y00 <pottq577@naver.com>
2025-04-01 14:33:29 +0900

    Feat: [Lv2] 유저 선택 조회 컨트롤러 구현 및 API 설계 완료

--- src/main/java/com/example/springtododevelop/controller/UserController.java
+++ src/main/java/com/example/springtododevelop/controller/UserController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,7 +39,7 @@ public class UserController {
             );
 
         return new ResponseEntity<>(userResponseDto, HttpStatus.CREATED);
-        
+
     }
 
     /**
@@ -53,6 +54,11 @@ public class UserController {
 
         return new ResponseEntity<>(users, HttpStatus.OK);
 
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponseDto> findUserById(@PathVariable Long userId) {
+        return new ResponseEntity<>(userService.findById(userId), HttpStatus.OK);
     }
 
 }
