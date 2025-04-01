37384fc9dd255fa7a95d8104fb7c9a63c9cbc716
hyun2y00 <pottq577@naver.com>
2025-04-01 14:21:13 +0900

    Feat: [Lv2] 유저 선택 조회 레포지토리 설계

--- src/main/java/com/example/springtododevelop/repository/UserRepository.java
+++ src/main/java/com/example/springtododevelop/repository/UserRepository.java
@@ -2,7 +2,16 @@ package com.example.springtododevelop.repository;
 
 import com.example.springtododevelop.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
+
+    default Users findByUserIdOrElseThrow(Long id) {
+        return findById(id).orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "해당 id (" + id + ")를 가진 유저가 없습니다."));
+
+    }
 
 }
