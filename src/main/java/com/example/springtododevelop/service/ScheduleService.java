4d0255f746db897c9d001a558967f1f71b6af361
hyun2y00 <pottq577@naver.com>
2025-04-02 11:44:47 +0900

    Refactor: 일정 서비스에서 팩토리 메소드 활용하도록 수정

--- src/main/java/com/example/springtododevelop/service/ScheduleService.java
+++ src/main/java/com/example/springtododevelop/service/ScheduleService.java
@@ -5,8 +5,8 @@ import com.example.springtododevelop.entity.Schedules;
 import com.example.springtododevelop.entity.Users;
 import com.example.springtododevelop.repository.ScheduleRepository;
 import com.example.springtododevelop.repository.UserRepository;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -34,14 +34,7 @@ public class ScheduleService {
 
         Schedules createdSchedule = scheduleRepository.save(schedule);
 
-        return new ScheduleResponseDto(
-            createdSchedule.getScheduleId(),
-            findUser.getUserId(),
-            createdSchedule.getTitle(),
-            createdSchedule.getContents(),
-            createdSchedule.getCreatedAt(),
-            createdSchedule.getUpdatedAt()
-        );
+        return ScheduleResponseDto.toDto(createdSchedule);
 
     }
 
@@ -52,25 +45,9 @@ public class ScheduleService {
      * @return 저장된 일정 정보가 담겨있는 {@link ScheduleResponseDto} 객체 리스트
      */
     public List<ScheduleResponseDto> findAllSchedules() {
-
-        List<Schedules> schedulesList = scheduleRepository.findAll();
-        List<ScheduleResponseDto> scheduleResponseDtoList = new ArrayList<>();
-
-        for (Schedules schedules : schedulesList) {
-            scheduleResponseDtoList.add(
-                new ScheduleResponseDto(
-                    schedules.getUser().getUserId(),
-                    schedules.getScheduleId(),
-                    schedules.getTitle(),
-                    schedules.getContents(),
-                    schedules.getCreatedAt(),
-                    schedules.getUpdatedAt()
-                )
-            );
-        }
-
-        return scheduleResponseDtoList;
-
+        return scheduleRepository.findAll().stream()
+            .map(ScheduleResponseDto::toDto)
+            .collect(Collectors.toList());
     }
 
 }
