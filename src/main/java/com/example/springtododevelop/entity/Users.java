85c8dc9c0523363668db455f3af0b55237f7233d
hyun2y00 <pottq577@naver.com>
2025-04-03 17:03:23 +0900

    Fix: Users 엔티티에서 schedules 리스트 제거

--- src/main/java/com/example/springtododevelop/entity/Users.java
+++ src/main/java/com/example/springtododevelop/entity/Users.java
@@ -1,15 +1,11 @@
 package com.example.springtododevelop.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -36,9 +32,6 @@ public class Users extends BaseEntity {
     @Column(nullable = false)
     @Setter
     private String email;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Schedules> schedules = new ArrayList<>();
 
     public Users(String username, String password, String email) {
         this.username = username;
