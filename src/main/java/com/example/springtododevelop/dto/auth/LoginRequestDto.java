c14956b65c674e9ec3b2aceffe0e2305a84f2f5a
hyun2y00 <pottq577@naver.com>
2025-04-03 15:07:17 +0900

    Feat: 로그인 응답/요청 DTO 설계

--- src/main/java/com/example/springtododevelop/dto/auth/LoginRequestDto.java
+++ src/main/java/com/example/springtododevelop/dto/auth/LoginRequestDto.java
@@ -1,4 +1,13 @@
 package com.example.springtododevelop.dto.auth;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class LoginRequestDto {
+
+    private final String email;
+    private final String password;
+
 }
--- src/main/java/com/example/springtododevelop/dto/auth/LoginResponseDto.java
+++ src/main/java/com/example/springtododevelop/dto/auth/LoginResponseDto.java
@@ -1,4 +1,21 @@
 package com.example.springtododevelop.dto.auth;
 
+import com.example.springtododevelop.entity.Users;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class LoginResponseDto {
+
+    private final String email;
+    private final String password;
+
+    public static LoginResponseDto toDto(Users users) {
+        return new LoginResponseDto(
+            users.getEmail(),
+            users.getPassword()
+        );
+    }
+
 }
