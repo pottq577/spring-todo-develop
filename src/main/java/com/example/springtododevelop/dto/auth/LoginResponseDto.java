f5cd50e096298661cf00f57d3d4baea2170c8981
hyun2y00 <pottq577@naver.com>
2025-04-03 15:33:10 +0900

    Feat: 로그인 응답 DTO 필드 추가

--- src/main/java/com/example/springtododevelop/dto/auth/LoginResponseDto.java
+++ src/main/java/com/example/springtododevelop/dto/auth/LoginResponseDto.java
@@ -1,6 +1,8 @@
 package com.example.springtododevelop.dto.auth;
 
+import com.example.springtododevelop.entity.Schedules;
 import com.example.springtododevelop.entity.Users;
+import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,13 +10,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LoginResponseDto {
 
-    private final String email;
+    private final Long userId;
+    private final String username;
     private final String password;
+    private final String email;
+    private final List<Schedules> schedules;
 
     public static LoginResponseDto toDto(Users users) {
         return new LoginResponseDto(
+            users.getUserId(),
+            users.getUsername(),
+            users.getPassword(),
             users.getEmail(),
-            users.getPassword()
+            users.getSchedules()
         );
     }
 
