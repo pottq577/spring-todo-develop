df8c96c0affb2c5ae50d08577e838e92c7118ede
hyun2y00 <pottq577@naver.com>
2025-04-04 11:13:35 +0900

    Refactor: 일정 정보가 없을 때 커스텀 예외 발생 및 활용하도록 수정

--- src/main/java/com/example/springtododevelop/exception/ExceptionCode.java
+++ src/main/java/com/example/springtododevelop/exception/ExceptionCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum ExceptionCode {
     PASSWORD_INVALID(403, "USER_001", "비밀번호가 일치하지 않습니다."),
     NOT_LOGIN_ERROR(401, "AUTH_001", "로그인을 먼저 해주세요."),
-    NOT_VALID_ERROR(400, "G011", "Validation Exception 발생");
+    NOT_VALID_ERROR(400, "G011", "Validation Exception 발생"),
+    SCHEDULE_NOT_FOUND(404, "SCHEDULE_001", "일정 정보를 찾을 수 없습니다.");
 
     private final int status;
     private final String code;
--- src/main/java/com/example/springtododevelop/repository/ScheduleRepository.java
+++ src/main/java/com/example/springtododevelop/repository/ScheduleRepository.java
@@ -1,16 +1,15 @@
 package com.example.springtododevelop.repository;
 
 import com.example.springtododevelop.entity.Schedules;
+import com.example.springtododevelop.exception.BusinessException;
+import com.example.springtododevelop.exception.ExceptionCode;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 public interface ScheduleRepository extends JpaRepository<Schedules, Long> {
 
     default Schedules findByScheduleIdOrElseThrow(Long id) {
         return findById(id).orElseThrow(
-            () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                "해당 id (" + id + ")를 가진 일정이 없습니다."));
+            () -> new BusinessException(ExceptionCode.SCHEDULE_NOT_FOUND));
 
     }
 
