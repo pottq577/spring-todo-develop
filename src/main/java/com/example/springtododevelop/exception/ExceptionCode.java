68e433406f39a1c672fb16d08a8b975259a61064
hyun2y00 <pottq577@naver.com>
2025-04-04 11:20:49 +0900

    Refactor: 커스텀 예외 code 간소화

--- src/main/java/com/example/springtododevelop/exception/ExceptionCode.java
+++ src/main/java/com/example/springtododevelop/exception/ExceptionCode.java
@@ -7,16 +7,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ExceptionCode {
     // 유저
-    PASSWORD_INVALID(403, "USER_001", "비밀번호가 일치하지 않습니다."),
-    USER_NOT_FOUND(404, "USER_002", "유저 정보를 찾을 수 없습니다."),
-    EMAIL_NOT_FOUND(404, "USER_003", "이메일이 존재하지 않습니다."),
+    PASSWORD_INVALID(403, "U001", "비밀번호가 일치하지 않습니다."),
+    USER_NOT_FOUND(404, "U002", "유저 정보를 찾을 수 없습니다."),
+    EMAIL_NOT_FOUND(404, "U003", "이메일이 존재하지 않습니다."),
 
     // 일정
-    SCHEDULE_NOT_FOUND(404, "SCHEDULE_001", "일정 정보를 찾을 수 없습니다."),
+    SCHEDULE_NOT_FOUND(404, "S001", "일정 정보를 찾을 수 없습니다."),
 
     // 공통
-    NOT_LOGIN_ERROR(401, "AUTH_001", "로그인을 먼저 해주세요."),
-    NOT_VALID_ERROR(400, "G011", "Validation Exception 발생");
+    NOT_LOGIN_ERROR(401, "A001", "로그인을 먼저 해주세요."),
+    NOT_VALID_ERROR(400, "R001", "Validation Exception 발생");
 
     private final int status;
     private final String code;
